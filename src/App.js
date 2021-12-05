e58871fd20006da07a1f7d74eff7cb0934e19ac4
Your Name <bartosz.milew@gmail.com>
2021-12-05 22:11:40 +0100

    add first layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,98 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from '@mui/material/Container';
+import { Button, ButtonGroup, Card, Grid, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [maxRandomElement, setMaxRandomElement] = useState(0);
+  const [resourceName, setResourceName] = useState('people');
+  const [playersInformation, setPlayersInformation] = useState({});
+
+  const getInformationDetailsFromApi = (randomNumber) => {
+    return fetch(`https://swapi.dev/api/${resourceName}/${randomNumber}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('error');
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => data)
+      .catch((error) => {
+        return getInformationDetailsFromApi(getRandomNumber());
+      });
+  };
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * maxRandomElement);
+  };
+
+  useEffect(() => {
+    //Get max element in poeple Api
+    getInformationDetailsFromApi('').then((response) =>
+      setMaxRandomElement(response.count)
+    );
+  }, []);
+
+  const handleClickPlay = () => {
+    Promise.all([
+      getInformationDetailsFromApi(getRandomNumber()),
+      getInformationDetailsFromApi(getRandomNumber()),
+    ]).then((data) => {
+      setPlayersInformation({
+        playerOne: data[0],
+        playerTwo: data[1],
+      });
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container fixed>
+          <Typography
+            gutterBottom
+            color="secondary"
+            variant="H3"
+            style={{ display: 'block' }}
+          >
+            LET'S PLAY A GAME
+          </Typography>
+          {maxRandomElement && (
+            <Grid container spacing={4}>
+              <Grid item xs={6}>
+                <Typography
+                  gutterBottom
+                  color="primary"
+                  variant="body1"
+                  style={{ display: 'block' }}
+                >
+                  Player 1
+                </Typography>
+                <Card sx={{ minWidth: 275 }}>Player 1</Card>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography
+                  gutterBottom
+                  color="primary"
+                  variant="body1"
+                  style={{ display: 'block' }}
+                >
+                  Player 2
+                </Typography>
+                <Card sx={{ minWidth: 275 }}>Player 2</Card>
+              </Grid>
+            </Grid>
+          )}
+          <Grid>
+            <ButtonGroup
+              variant="contained"
+              aria-label="outlined primary button group"
+            >
+              <Button onClick={handleClickPlay}>Play</Button>
+              <Button>Settings</Button>
+            </ButtonGroup>
+          </Grid>
+        </Container>
       </header>
     </div>
   );
